e939c8deb9db9b997ccb7f3cd0812954a68f850b
Srinivas Peethala <speethala@aortem.io>
2026-03-08 18:33:46 +0530

    dd

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+from fastapi.middleware.cors import CORSMiddleware
 
 # Create FastAPI app
 app = FastAPI()
@@ -59,3 +60,19 @@ def sentiment_analysis(data: SentencesRequest):
         })
 
     return {"results": results}
+
+# CORS config
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],        # use `origins` instead of ["*"] to restrict
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
